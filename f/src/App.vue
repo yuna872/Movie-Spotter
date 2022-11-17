2b92ea442b98724b9e33b06f91c473a00fbf51a7
yuna kim <yunae426@gmail.com>
2022-11-17 22:47:37 +0900

    html+css ing

--- f/src/App.vue
+++ f/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Movies</router-link> |
-      <router-link to="/accounts/login">Login</router-link> |
-      <router-link to="/accounts/signup">Signup</router-link> |
-      <router-link to="/firsttime">FirstTime</router-link> |
-      <router-link to="/aboutus">About Us</router-link>
+      <div class="nav-left">
+        <img src="@/assets/logo.png" class="nav-logo">
+        <div class="nav-title">Movie Spotter</div>
+      </div>
+      <div class="nav-right">
+        <router-link :to="{ name : 'movies' }">Movies</router-link>
+        <router-link :to="{ name : 'login' }">Login</router-link>
+        <router-link :to="{ name : 'signup' }">Signup</router-link>
+        <router-link :to="{ name : 'firsttime' }">FirstTime</router-link>
+      </div>
       <!-- 유저 아이디에 해당하는 UserInfo 라우터 링크 -->
       <!-- <router-link :to="{ name : 'Userinfo', params: {id: user.id }}">My Page</router-link> -->
     </nav>
     <router-view @login="login"/>
+    <div class="about-us-btn" @click="toAboutUs"></div>
   </div>
 </template>
 
@@ -25,6 +31,9 @@ export default {
     login() {
       this.isLogin = true
       console.log(this.isLogin)
+    },
+    toAboutUs() {
+      this.$router.push({ name : 'aboutus' })
     }
   },
 }
@@ -36,20 +45,60 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: center; 
 }
 
+
+
+/* nav a.router-link-exact-active {
+} */
+
+
+/* 네비게이션 */
 nav {
-  padding: 30px;
+  height : 50px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #343440;
+  color : white;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  color : white;
+  text-decoration: none;
+  margin : 0 5px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.nav-left {
+  margin : auto 15px;
+  display: flex;
+}
+.nav-logo{
+  background-image: url('../src/assets/logo.png');
+  background-size: cover;
+  width : 35px;
+  height : 35px;
+  margin-right : 10px;
+}
+.nav-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav-right {
+  margin : 5px;
+  display: flex;
+  align-items: center;
+}
+
+/* AboutUs 버튼 */
+.about-us-btn{
+  width : 50px;
+  height : 50px;
+  background-color : #F6BE00;
+  position: fixed;
+  bottom : 20px;
+  right: 20px;
 }
 </style>
