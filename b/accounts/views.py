c26f8263e00b14506fa5beb020826b4e056ad4a2
choizlor <choizlor@gmail.com>
2022-11-17 23:05:48 +0900

    follow 기능 ing

--- b/accounts/views.py
+++ b/accounts/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.http.response import JsonResponse
+
 
 @api_view(['POST'])
 def signup(request):
@@ -24,6 +28,26 @@ def signup(request):
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['POST'])
 def login(request):
     pass
+
+
+@api_view(['POST'])
+def follow(request, username):
+    person = get_object_or_404(get_user_model(), username=username)
+    user = request.user
+
+    if person != user:
+        if person.followers.filter(pk=user.pk).exists():
+            person.followers.remove(user)
+            follow = True
+        else:
+            person.followers.add(user)
+            follow = False
+        follow_status = {
+            'follow': follow,
+            'count': person.followers.count(),
+        }
+        return JsonResponse(follow_status)
