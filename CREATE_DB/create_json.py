fc57976fa6e25645d1c0f0a10dc39c845680beb5
qkrtlgud3286 <qkrtlgud3286@naver.com>
2022-11-18 16:23:43 +0900

    create 배우, 예고편 정보 추가

--- CREATE_DB/create_json.py
+++ CREATE_DB/create_json.py
@@ -2,18 +2,61 @@ import requests
 import json
 
 TMDB_API_KEY = '633fd9754acba9ddc40773b19c562ed9'
+# def GET_CREDIT(movie_id):
+#     Get_Credit = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=633fd9754acba9ddc40773b19c562ed9&language=ko').json()
+
+#     actors = []
+#     for i in Get_Credit['crew']:
+#         if i['known_for_department'] == 'Directing':
+#             actors.append(i['name'])
+#             break
+#     for i in Get_Credit['cast']:
+#         actors.append(i['name'])
+#         if len(actors) == 4:
+#             break
+#     return actors
+
+
+# def GET_VIDEO(movie_id):
+#     Get_Video = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=633fd9754acba9ddc40773b19c562ed9&language=ko').json()
+#     if len(Get_Video['results']) == 0 or ('results' not in Get_Video.keys()):
+#         return 'novideo'
+#     else:
+#         return Get_Video['results'][0]['key']
+
+
 
 def get_movie_datas():
     total_data = []
 
     # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
-    for i in range(1, 500):
-        request_url = f'https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}'
+    for i in range(1, 3000):
+        request_url = f'https://api.themoviedb.org/3/movie/upcoming?api_key={TMDB_API_KEY}&language=ko-KR&page={i}'
         movies = requests.get(request_url).json()
 
         for movie in movies['results']:
             if movie.get('release_date', ''):
                 if movie.get('vote_average') > 7:
+                    # movie_id = movie['id']
+                    # Get_Credit = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=633fd9754acba9ddc40773b19c562ed9&language=ko').json()
+                    # actors = []
+                    # for i in Get_Credit['crew']:
+                    #     if i['known_for_department'] == 'Directing':
+                    #         actors.append(i['name'])
+                    #         break
+                    # for i in Get_Credit['cast']:
+                    #     actors.append(i['name'])
+                    #     if len(actors) == 4:
+                    #         break
+                    
+                    # video = ''
+                    # Get_Video = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=633fd9754acba9ddc40773b19c562ed9&language=ko').json()
+                    # if len(Get_Video['results']) == 0 or ('results' not in Get_Video.keys()):
+                    #     video = 'novideo'
+                    # else:
+                    #     video = Get_Video['results'][0]['key']
+
+
                     fields = {
                         'title': movie['title'],
                         'release_date': movie['release_date'],
@@ -25,6 +68,8 @@ def get_movie_datas():
                         'genres': movie['genre_ids'],
                         'original_language': movie['original_language'],
                         'vote_count': movie['vote_count'],
+                        # 'actors' : actors,
+                        # 'video' : video,
                     }
 
                     data = {
@@ -34,11 +79,11 @@ def get_movie_datas():
                     }
 
                     total_data.append(data)
-
             if len(total_data) == 50:
                 with open('movies.json', 'w', encoding='UTF-8') as file:
                     file.write(json.dumps(total_data, ensure_ascii=False)) 
                 return
+
 
 get_movie_datas()
 print('?')
