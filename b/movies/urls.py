5564914c856c882909e3eeb1ce5361f36280221b
choizlor <choizlor@gmail.com>
2022-11-21 00:02:35 +0900

    movie like 기능 ing

--- b/movies/urls.py
+++ b/movies/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # path('<int:movie_pk>/reviews/', views.review_create),
     path('reviews/<int:review_pk>/', views.review_detail),
     path('reviews/<int:user_pk>', views.my_reviews),
-    path("<int:movie_pk>/moivelikes/", views.movie_likes, name="movie_likes"),
+    path("<int:movie_pk>/movielikes/", views.movie_likes, name="movie_likes"),
     path("<int:review_pk>/reviewlikes/", views.review_likes, name="review_likes"),
     # # 필수 작성
     # path('schema/', SpectacularAPIView.as_view(), name='schema'),
--- f/src/components/MovieItem.vue
+++ f/src/components/MovieItem.vue
@@ -2,15 +2,22 @@
   <div class="movie-item" @click="goDetail" :style="{'backgroundImage':`url(${posterUrl})`}">
     {{ movie?.title }}
     {{ movie?.['vote_average'] }}
-    <button>좋아요 버튼</button>
+    <button @click="movieLike">{{ is_like }}</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'MovieItem',
   props: {
     movie : Object,
+  },
+  data() {
+    return {
+      is_like: null,
+      movieinfo : null,
+    }
   },
   beforeRouteUpdate(to, from, next){
     console.log(to.params)
@@ -30,7 +37,46 @@ export default {
   methods: {
     goDetail() {
       this.$router.push({name: 'detail', params: { id : this.movie.id }})
+    },
+    movieLike() {
+      const token = localStorage.getItem('jwt')
+      const now_user_id = jwt_decode(token).user_id
+
+      axios({
+          method: 'post',
+          url: `${API_URL}/movies/${this.movie.id}/movielikes/`,
+          data: {
+            user_id: now_user_id
+          },
+          headers: {
+            'Authorization' : `Bearer ${token}`
+          }
+        })
+        .then((res)=>{
+          this.is_like = res.data
+        })
+        .catch((err)=>{console.log(err)})
+    },
+    getMovieLikeInfo() {
+      const token = localStorage.getItem('jwt')
+      const now_user_id = jwt_decode(token).user_id
+
+      axios({
+        method: 'get',
+        url: `${API_URL}/movies/${movie.id}/`,
+      })
+      .then((res) => {
+        this.movieinfo = res.data
+        if (this.movieinfo.like_movies.includes(now_user_id)) {
+          this.is_like = false
+        } else {
+          this.is_like = true
+        }
+      })
     }
+  },
+  created() {
+    getMovieLikeInfo()
   }
 }
 </script>
@@ -39,7 +85,9 @@ export default {
 .movie-item {
   width : 16vw;
   height : 24vw;
-  background-size: cover;
+  background-size: cover; 
+  background-repeat: no-repeat;
+  background-position: center;
   margin-top : 10px;
 }
 </style>
