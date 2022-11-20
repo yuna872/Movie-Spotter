8109c57303dbda536a5b29b6d1eee7d26178b81c
choizlor <choizlor@gmail.com>
2022-11-20 22:46:55 +0900

    follow 기능 ing

--- f/src/views/accounts/UserInfo.vue
+++ f/src/views/accounts/UserInfo.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="user-info">
     <h3>회원 정보 페이지</h3>
-    <p>{{userinfo.username}}님 안녕하세요?</p>
-    <p>제 닉네임은 {{userinfo.nickname}} 입니다.</p>
-    <p>팔로워: {{userinfo.followers.length}} | 팔로잉: {{userinfo.followings.length}}</p>
-    <p>팔로워들 : {{userinfo.followers}}</p>
-    <button @click='follow'>{{is_follow}}</button>
+    <p>{{ userinfo.username }}님 안녕하세요?</p>
+    <p>제 닉네임은 {{ userinfo.nickname }} 입니다.</p>
+    <p>팔로워: {{ userinfo.followers.length }} | 팔로잉: {{ userinfo.followings.length }}</p>
+    <p>팔로워: {{ this.followersLength }} | 팔로잉: {{ userinfo.followings.length }}</p>
+    <p>팔로워들 : {{ userinfo.followers }}</p>
+    <button @click='follow'>{{ is_follow }}</button>
   </div>
 </template>
 
@@ -26,6 +27,8 @@ export default {
   },
   created() {
     const token = localStorage.getItem('jwt')
+    const now_user_id = jwt_decode(token).user_id
+
     axios({
         method: 'get',
         url: `${API_URL}/accounts/${this.$route.params.id}`,
@@ -35,6 +38,11 @@ export default {
       })
       .then((res)=>{
         this.userinfo = res.data
+        if (this.userinfo.followers.includes(now_user_id)) {
+          this.is_follow = 'UnFollow'
+        } else {
+          this.is_follow = 'follow'
+        }
         console.log(this.userinfo)
       })
       .catch((err)=>{console.log(err)})
@@ -57,14 +65,24 @@ export default {
       })
         .then((res) => {
           if (res.data === true){
-            this.is_follow = '언팔로우'
+            this.is_follow = 'UnFollow'
           } else if (res.data === false) {
-            this.is_follow = '팔로우'
-          }       
+            this.is_follow = 'Follow'
+          }
         })
         .catch((err)=>{console.log(err)})
-
-      
+    },
+    computed: {
+      followersLength() {
+        return this.userinfo?.followers.length
+      },
+      followingsLength() {
+        if (this.userinfo.followings) {
+          return this.userinfo.followings.length
+        } else {
+          return 0
+        }
+      },
     }
   
   }
