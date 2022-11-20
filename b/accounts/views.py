f1d9dd7455e3e6bb5776a24848b827910b4147fd
choizlor <choizlor@gmail.com>
2022-11-20 15:15:57 +0900

    hotfix signup bug

--- b/accounts/views.py
+++ b/accounts/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer, SignupSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from django.http.response import JsonResponse
@@ -24,7 +23,7 @@ def signup(request):
             # print(user)
             user.set_password(password)
             user.save()
-
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
